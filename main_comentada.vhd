b203a8ffef57e5c0685ca7f14429b0c4c22e7854
Ana Schenkel <68713405+Ana-Schenkel@users.noreply.github.com>
2025-07-06 20:10:26 -0300

    Update and rename main_comentada to main_comentada.vhd

--- main_comentada.vhd
+++ main_comentada.vhd
@@ -4,13 +4,13 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity main is
   port (
-    clk      	: in std_logic;
-    ps2_clk  	: in std_logic;
-    ps2_data 	: in std_logic;
-    RS, RW   	: out bit;
-    E        	: buffer bit;
-    DB       	: out std_logic_vector(7 downto 0);
-	 leds			: out std_logic_vector (7 downto 0)
+	clk      	: in std_logic;
+	ps2_clk  	: in std_logic;
+	ps2_data 	: in std_logic;
+	RS, RW   	: out bit;
+	E        	: buffer bit;
+	DB       	: out std_logic_vector(7 downto 0);
+	leds		: out std_logic_vector (7 downto 0)
 	 );
 end main;
 
@@ -19,11 +19,11 @@ architecture Behavioral of main is
 	-- Components from each file that are connected to the main module
 	component key is
 	 port (
-		data			: in std_logic;
-		pclk			: in std_logic;
-		clk			: in std_logic;
-		key_pulse   : out std_logic;
-		key_press   : out integer
+		data		: in std_logic;
+		pclk		: in std_logic;
+		clk		: in std_logic;
+		key_pulse   	: out std_logic;
+		key_press   	: out integer
 	 );
 	end component;
 
@@ -31,10 +31,10 @@ architecture Behavioral of main is
 	 port (
 		line_0		: in string(1 to 16);
 		line_1		: in string(1 to 16);
-		clk			: in std_logic;
+		clk		: in std_logic;
 		RS, RW		: out bit;
-		E				: buffer bit;
-		DB				: out std_logic_vector(7 downto 0)
+		E		: buffer bit;
+		DB		: out std_logic_vector(7 downto 0)
 	 );
 	end component;
   
@@ -49,11 +49,14 @@ architecture Behavioral of main is
 	);
 	end component;
 
+	-- Auxiliary signals for the connection between proc_matrix.vhd and LCD.vhd
 	signal key_press 	: integer;
 	signal key_pulse 	: std_logic;
 	signal line_0		: string(1 to 16) :=  "                ";
 	signal line_1		: string(1 to 16) :=  "                ";
-	signal game_level	: integer;
+	
+	-- Signals for the LEDs logic
+	signal game_level	: integer; -- Output from the proc_matrix.vhd file
 	signal leds_s		: std_logic_vector (7 downto 0);
 	signal counter		: integer range 0 to 49_999_999 := 0;
 	signal flash_leds_s : std_logic := '0';
@@ -62,16 +65,16 @@ begin
 
 -- DUT of KEY
   key_dut : key port map (
-      data => ps2_data,
-      pclk => ps2_clk,
+      		data => ps2_data,
+      		pclk => ps2_clk,
 		clk => clk,
-      key_press => key_press,
+      		key_press => key_press,
 		key_pulse => key_pulse
     );
 	 
 -- DUT of proc_matrix      
   matrix_dut : proc_matrix port map (
-      clk => clk,
+      		clk => clk,
 		key_press => key_press,
 		key_pulse => key_pulse,
 		line_0 => line_0,
@@ -81,13 +84,13 @@ begin
 
 -- DUT of LCD      
   lcd_dut : LCD port map (
-      line_0 => line_0,
+      		line_0 => line_0,
 		line_1 => line_1,
 		clk => clk,
-      RS => RS,
-      RW => RW,
-      E  => E,
-      DB => DB
+      		RS => RS,
+      		RW => RW,
+      		E  => E,
+      		DB => DB
     );
 	 
 leds <= leds_s;
